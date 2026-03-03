87428ecf85d97316c29358a706ddd9001fb56f2f
Admin <kareemoii37@gmail.com>
2026-03-03 09:48:31 +0200

    create: client entity.

---
+++ src/main/java/com/nti/paymentservice/entity/ClientEntity.java
@@ -0,0 +1,23 @@
+package com.nti.paymentservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "clients" )
+@Getter
+@Setter
+public class ClientEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String username;
+    @Column(nullable = false, length = 100)
+    private String password;
+    @Column(nullable = false)
+    private UUID key;
+}
